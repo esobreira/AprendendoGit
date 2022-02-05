70c12488dc3fe9578bcfd26b269128feac286caf
Eberton Sobreira <eberton@outlook.com>
2022-02-05 01:04:45 -0300

    Acerto do Program.cs pra exibir meu nome

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("==========================================================");
+﻿using AprendendoGit;
+
+Console.WriteLine("==========================================================");
 Console.WriteLine("                      Hello, World!");
 Console.WriteLine("==========================================================");
+
+var reg1 = new Registro()
+{
+    Id = 1,
+    DataNascimento = new DateTime(1982, 2, 28),
+    Nome = "Eberton Sobreira"
+};
+
+Console.WriteLine($"ID: {reg1.Id}, {reg1.Nome}, {reg1.DataNascimento.ToShortDateString()}");
