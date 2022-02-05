423022e5855a957e160f9988b087178c5faa9d20
Eberton Sobreira <eberton@outlook.com>
2022-02-05 00:14:33 -0300

    Organizacao de arquivos

---
+++ Registro.cs
@@ -0,0 +1,9 @@
+namespace AprendendoGit
+{
+    public class Registro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = "";
+        public DateTime DataNascimento { get; set; }
+    }
+}
---
+++ Teste.cs
@@ -0,0 +1,7 @@
+namespace AprendendoGit {
+    public static class Teste {
+        public static int GetTeste() {
+            return 0;
+        }
+    }
+}
