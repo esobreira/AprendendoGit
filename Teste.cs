f57afcbfe65a53c57704011d5f6ce91316f8112f
Eberton Sobreira <eberton@outlook.com>
2022-02-05 00:41:07 -0300

    Tentando baguncar

--- Teste.cs
+++ Teste.cs
@@ -1,10 +1,19 @@
 namespace AprendendoGit {
     public static class Teste {
+        
         public static int GetTeste() {
             return 0;
         }
         public static int GetTesteAvancado() {
             return 1;
+        }
+
+        public static int GetTesteAvancado2() {
+            return 22222;
+        }
+
+        public static int GetTesteAvancado3() {
+            return 3;
         }
     }
 }
